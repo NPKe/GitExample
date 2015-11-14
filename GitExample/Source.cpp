329d459d955cb88363f957234ce3d4f541701f58
Ke Nguyen <kenguyen276@gmail.com>
2015-11-15 00:24:50 +0700

    Add function done

---
+++ GitExample/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int a, b;
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << Add(a, b) << endl;
+	return 0;
+}
